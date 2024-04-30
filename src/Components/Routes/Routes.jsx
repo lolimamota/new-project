378256f3b1e0653bc42a92db4505e325f9dfcf1a
lolimamota <lorraynelimamota@gmail.com>
2024-04-30 18:05:11 -0300

    inclusão de estilos na nav

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -2,17 +2,18 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from '../Homepage/Home.jsx';
 import About from '../Aboutpage/About.jsx';
 import Projects from '../Projectspage/Projects.jsx';
+import * as S from './RoutesStyle.jsx';
 
 export default function Router(){
     return(
         <BrowserRouter>
-       <nav>
+       <S.NavBar>
         <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/About'>About</Link></li>
-            <li><Link to='/Projects'>Projects</Link></li>
+            <li><Link className="backgroundNavBar" to='/'>Home</Link></li>
+            <li><Link className="backgroundNavBar" to='/About'>About</Link></li>
+            <li><Link className="backgroundNavBar" to='/Projects'>Projects</Link></li>
         </ul>
-       </nav>
+       </S.NavBar>
        <Routes>
        <Route path="/" element={<Home />} />
        <Route path="/About" element={<About />} />
---
+++ src/Components/Routes/RoutesStyle.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const NavBar = styled.nav`
+    font-family: "Sevillana", cursive;
+    font-weight: 400;
+    width: 25%;
+    height: 100vh;
+    background-color: #1E555C;
+    li, .backgroundNavBar{
+        background-color: #1E555C;
+    }
+`;
