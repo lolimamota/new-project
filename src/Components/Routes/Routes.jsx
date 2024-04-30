d85ce6574510304036dd32e4926c35e9d24e6192
lolimamota <lorraynelimamota@gmail.com>
2024-04-30 18:19:04 -0300

    commit duplo de retirada de icon

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -4,22 +4,21 @@ import About from '../Aboutpage/About.jsx';
 import Projects from '../Projectspage/Projects.jsx';
 import * as S from './RoutesStyle.jsx';
 
-export default function Router(){
-    return(
+export default function Router() {
+    return (
         <BrowserRouter>
-       <S.NavBar>
-        
-        <ul>
-            <li><Link className="backgroundNavBar" to='/'>Home</Link></li>
-            <li><Link className="backgroundNavBar" to='/About'>About</Link></li>
-            <li><Link className="backgroundNavBar" to='/Projects'>Projects</Link></li>
-        </ul>
-       </S.NavBar>
-       <Routes>
-       <Route path="/" element={<Home />} />
-       <Route path="/About" element={<About />} />
-       <Route path="/Projects" element={<Projects />} />
-       </Routes>
-       </BrowserRouter>
+            <S.NavBar>
+                <ul>
+                    <li><Link className="backgroundNavBar" to='/'>Home</Link></li>
+                    <li><Link className="backgroundNavBar" to='/About'>About</Link></li>
+                    <li><Link className="backgroundNavBar" to='/Projects'>Projects</Link></li>
+                </ul>
+            </S.NavBar>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/About" element={<About />} />
+                <Route path="/Projects" element={<Projects />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
